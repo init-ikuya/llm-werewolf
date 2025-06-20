f0588cf6cee6fbdf5a9fc8894441df40780d3f2a
initiate <76862995+init-ikuya@users.noreply.github.com>
2025-06-20 22:49:06 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: process.env.NODE_ENV === 'production' ? '/llm-werewolf/' : '/',
+  test: {
+    environment: 'jsdom',
+    globals: true
+  }
 })
